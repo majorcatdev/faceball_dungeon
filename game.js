c82060b493970ee72e43cb83ee1edf376cdc940e
majorcatdev <120533675+majorcatdev@users.noreply.github.com>
2023-02-09 16:20:27 -0800

    Update game.js

--- game.js
+++ game.js
@@ -58,6 +58,23 @@ function startGame(){
    
 }
 
+function rectCollision(rect1, rect2){
+    return (
+        rect1.x < rect2.x + rect2.w &&
+        rect1.x + rect1.w > rect2.x &&
+        rect1.y < rect2.y + rect2.h &&
+        rect1.h + rect1.y > rect2.y
+    ) 
+      
+}
+
+function circleCollision(circle1, circle2){
+    const dx = circle1.x - circle2.x;
+    const dy = circle1.y - circle2.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return (distance < circle1.radius + circle2.radius);
+}
+
 
 
 let Game ={
@@ -159,4 +176,4 @@ function update(){
     updateSprites();
 }
 
-	
+	//make ball falling collecting game
